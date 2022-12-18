3722523cf8630c60480398cb549ee991b05ada64
KarVarr <megatr9n@gmail.com>
2022-12-18 13:01:53 +0300

    added mterics height

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -55,8 +55,9 @@ class ViewController: UIViewController {
             
             view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
+        let metrics = ["labelHieght": 88]
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]|", options: [],  metrics: nil, views: viewsDictionary))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHieght@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [],  metrics: metrics, views: viewsDictionary))
         
         
     }
