a6c68f78d347f54785ec7785d301a1ad3e4c25c9
KarVarr <megatr9n@gmail.com>
2022-12-21 17:07:19 +0300

    Finished the project

--- Project7/Project7/DetailViewController.swift
+++ Project7/Project7/DetailViewController.swift
@@ -16,19 +16,34 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         wevView = WKWebView()
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let detailItem = detailItem else { return }
+        
+        let html = """
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style> body { font-size: 150%; } </style>
+        </head>
+        <body>
+        \(detailItem.body)
+        </body>
+        </html>
+        """
+        
+        
+        wevView.loadHTMLString(html, baseURL: nil)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -14,14 +14,32 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString =
+                "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString =
+                "https://api.whitehouse.gov/v1/petitions.json?signatureConuntFloor=10000&limit=199"
+        }
         
         if let url = URL(string:  urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+            } else {
+                showError()
             }
+        } else {
+            showError()
         }
         
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data)
@@ -44,6 +62,12 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
     
 
