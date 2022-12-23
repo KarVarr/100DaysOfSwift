df8dc81dbcb7bfe7f8bded221abe008fea901105
KarVarr <megatr9n@gmail.com>
2022-12-23 17:50:00 +0300

    added the project 8

---
+++ Projaect8/Projaect8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projaect8
+//
+//  Created by Karen Vardanian on 23.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
