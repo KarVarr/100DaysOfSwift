fadcb0c646992b4280c9af8bd100c2df4fd98b76
KarVarr <megatr9n@gmail.com>
2022-12-29 18:32:34 +0300

    project 10 UICollectionView. Added UICollectionView and navigation

---
+++ Project10/Project10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by Karen Vardanian on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
