457fb1549d0f35ae5f9744a0090e74cef5de7a6f
KarVarr <megatr9n@gmail.com>
2022-12-29 18:52:35 +0300

    added PersonCell UICollectionViewCell and changed a color for veiw background

--- Project10/Project10/ViewController.swift
+++ Project10/Project10/ViewController.swift
@@ -14,6 +14,18 @@ class ViewController: UICollectionViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to deqeue PersonCell")
+        }
+        return cell
+    }
+    
+    
 
 }
 
