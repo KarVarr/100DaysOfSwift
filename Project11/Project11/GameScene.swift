ccde88a2aeaa8d242daf6eac138d8a13d39fa3b4
KarVarr <megatr9n@gmail.com>
2023-01-01 17:01:28 +0300

    added ball and bouncers, plus physics

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -16,35 +16,57 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))
    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {return}
         let location = touch.location(in: self)
         
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-        box.position = location
-        addChild(box)
+//        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+//        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+//        box.position = location
+//        addChild(box)
+        
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+        ball.physicsBody?.restitution = 0.5
+        ball.position = location
+        addChild(ball)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {return}
-        let location = touch.location(in: self )
-        
-        let box = SKSpriteNode(color: .blue, size: CGSize(width: 32, height: 32))
-        box.position = location
-        addChild(box)
+    func makeBouncer(at position: CGPoint)  {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody?.isDynamic = false
+        bouncer.position = position
+        addChild(bouncer)
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {return}
-        let location = touch.location(in: self)
-        
-        let box = SKSpriteNode(color: .magenta, size: CGSize(width: 24, height: 24))
-        box.position = location
-        addChild(box)
-    }
+    
+    
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard let touch = touches.first else {return}
+//        let location = touch.location(in: self )
+//
+//        let box = SKSpriteNode(color: .blue, size: CGSize(width: 32, height: 32))
+//        box.position = location
+//        addChild(box)
+//    }
+//
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard let touch = touches.first else {return}
+//        let location = touch.location(in: self)
+//
+//        let box = SKSpriteNode(color: .magenta, size: CGSize(width: 24, height: 24))
+//        box.position = location
+//        addChild(box)
+//    }
    
     
 }
