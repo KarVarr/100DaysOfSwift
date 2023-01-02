150ba362af4ea3f3b67f984e511d8239136f4c0e
KarVarr <megatr9n@gmail.com>
2023-01-02 12:34:21 +0300

    added box and edition mode

--- Project11/Project11/GameScene.swift
+++ Project11/Project11/GameScene.swift
@@ -8,12 +8,42 @@
 import SpriteKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    var scoreLabel: SKLabelNode!
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
+    
+    var editLabel: SKLabelNode!
+    var editingMode: Bool = false {
+        didSet {
+            if editingMode {
+                editLabel.text = "Done!"
+            } else {
+                editLabel.text = "Edit"
+            }
+        }
+    }
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "background")
         background.position = CGPoint(x: 512, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
         addChild(background)
+        
+        scoreLabel = SKLabelNode(fontNamed:  "Chalkduster")
+        scoreLabel.text = "Score: 0"
+        scoreLabel.horizontalAlignmentMode = .right
+        scoreLabel.position = CGPoint(x: 980, y: 700)
+        addChild(scoreLabel)
+        
+        editLabel = SKLabelNode(fontNamed:  "Chalkduster")
+        editLabel.text = "Edit"
+        editLabel.position = CGPoint(x: 80 , y: 700)
+        addChild(editLabel)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         physicsWorld.contactDelegate = self
@@ -29,24 +59,43 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         makeBouncer(at: CGPoint(x: 768, y: 0))
         makeBouncer(at: CGPoint(x: 1024, y: 0))
         
-   }
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else {return}
         let location = touch.location(in: self)
+        let objects = nodes(at: location)
         
-//        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
-//        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-//        box.position = location
-//        addChild(box)
+        if objects.contains(editLabel) {
+            editingMode.toggle()
+        } else {
+            if editingMode {
+                let size = CGSize(width: Int.random(in: 16...128), height: 16)
+                let box = SKSpriteNode(color: UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1), size: size)
+                box.zRotation = CGFloat.random(in: 0...3)
+                box.position = location
+                
+                box.physicsBody = SKPhysicsBody(rectangleOf: box.size)
+                box.physicsBody?.isDynamic = false
+                addChild(box)
+            } else {
+                let ball = SKSpriteNode(imageNamed: "ballRed")
+                ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+                ball.physicsBody?.restitution = 0.5
+                ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
+                ball.position = location
+                ball.name = "ball"
+                addChild(ball)
+            }
+        }
         
-        let ball = SKSpriteNode(imageNamed: "ballRed")
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
-        ball.physicsBody?.restitution = 0.5
-        ball.physicsBody?.contactTestBitMask = ball.physicsBody?.collisionBitMask ?? 0
-        ball.position = location
-        ball.name = "ball"
-        addChild(ball)
+        //        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
+        //        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+        //        box.position = location
+        //        addChild(box)
+        
+        
+        
     }
     
     func makeBouncer(at position: CGPoint)  {
@@ -88,8 +137,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func collision (between ball: SKNode, object: SKNode) {
         if object.name == "good" {
             destroy(ball: ball)
+            score += 1
         } else if object.name == "bad" {
             destroy(ball: ball)
+            score -= 1
         }
     }
     
@@ -111,24 +162,24 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     
     
-//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        guard let touch = touches.first else {return}
-//        let location = touch.location(in: self )
-//
-//        let box = SKSpriteNode(color: .blue, size: CGSize(width: 32, height: 32))
-//        box.position = location
-//        addChild(box)
-//    }
-//
-//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        guard let touch = touches.first else {return}
-//        let location = touch.location(in: self)
-//
-//        let box = SKSpriteNode(color: .magenta, size: CGSize(width: 24, height: 24))
-//        box.position = location
-//        addChild(box)
-//    }
-   
+    //    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //        guard let touch = touches.first else {return}
+    //        let location = touch.location(in: self )
+    //
+    //        let box = SKSpriteNode(color: .blue, size: CGSize(width: 32, height: 32))
+    //        box.position = location
+    //        addChild(box)
+    //    }
+    //
+    //    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //        guard let touch = touches.first else {return}
+    //        let location = touch.location(in: self)
+    //
+    //        let box = SKSpriteNode(color: .magenta, size: CGSize(width: 24, height: 24))
+    //        box.position = location
+    //        addChild(box)
+    //    }
+    
     
 }
 
