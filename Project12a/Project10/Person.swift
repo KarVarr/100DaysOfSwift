5ad15ed646536e7db6e5658be839dc32cae719f6
KarVarr <megatr9n@gmail.com>
2023-01-04 17:05:06 +0300

    lesson 12. added two copy of project 10

---
+++ Project12a/Project10/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Karen Vardanian on 30.12.2022.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name : String
+    var image : String
+    
+    internal init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ Project12a/Project10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Karen Vardanian on 29.12.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
