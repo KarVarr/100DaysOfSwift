b9943b687dea44b23af089b749131a6df0212c78
KarVarr <megatr9n@gmail.com>
2023-01-07 15:55:44 +0300

    added map and flatmap

--- MyPlaygroundUIKit.playground/Contents.swift
+++ MyPlaygroundUIKit.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
+//MARK: - map
+var number = [4.0, 9.0 ,16.0]
+var final = number.map(sqrt)
 
-var greeting = "Hello, playground"
+//MARK: - flatMap
+var flatnum = ["1", "5", "non"]
+var final2 = flatnum.compactMap{Int($0)}
+print(final2)
+print(type(of: final2))
+
+let num: String? = "5"
+let final3 = num.map{Int($0)}
+type(of: final3)
+let final4 = num.flatMap{Int($0)}
+type(of: final4)
+
+//MARK: - filter
+
