7bf55c186ae7e1ac489a4d681fc51f0978363785
KarVarr <megatr9n@gmail.com>
2023-01-08 12:46:16 +0300

    added project 13 UISlider

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Karen Vardanian on 08.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
