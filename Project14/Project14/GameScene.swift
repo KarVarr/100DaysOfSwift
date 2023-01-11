99fee428ef719417c7aed09f2b3fa91b38a9e94a
KarVarr <megatr9n@gmail.com>
2023-01-11 17:00:48 +0300

    added project 14 game for iPad

---
+++ Project14/Project14/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  Project14
+//
+//  Created by Karen Vardanian on 11.01.2023.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+      
+    }
+  
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        }
+   
+}
