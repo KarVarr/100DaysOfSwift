d22699056b2138e52aaf52fbe566a1960efef513
KarVarr <megatr9n@gmail.com>
2023-01-12 18:15:59 +0300

    added time and sounds

--- Project14/Project14/GameScene.swift
+++ Project14/Project14/GameScene.swift
@@ -13,6 +13,7 @@ class GameScene: SKScene {
     var gameScore: SKLabelNode!
     
     var popupTime = 0.85
+    var numRounds = 0
     
     var score = 0 {
         didSet {
@@ -43,37 +44,78 @@ class GameScene: SKScene {
             self?.createEnemy()
         }
     }
-  
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return}
+        let location = touch.location(in: self)
+        let tappedNodes = nodes(at: location)
         
+        for node in tappedNodes {
+            guard let whackSlot = node.parent?.parent as? WhackSlot else {
+                continue
+            }
+            
+            if !whackSlot.isVisible {continue}
+            if whackSlot.isHit {continue}
+            whackSlot.hit()
+            
+            if node.name == "charFriend" {
+                score -= 5
+                run(SKAction.playSoundFileNamed("whackBad.caf",
+                                                waitForCompletion: false))
+                
+            } else if node.name == "charEnemy" {
+                whackSlot.charNode.xScale = 0.85
+                whackSlot.charNode.yScale = 0.85
+                score += 1
+                run(SKAction.playSoundFileNamed("whack.caf",
+                                                waitForCompletion: false))
+                
+                }
+            }
+            
         }
-    
-    func createSlot(at position: CGPoint) {
-        let slot = WhackSlot()
-        slot.configure(at: position)
-        addChild(slot)
-        slots.append(slot)
-    }
-    
-    func createEnemy ( ) {
-        popupTime *= 0.991
         
-        slots.shuffle()
-        slots[0].show(hideTime: popupTime)
-        
-        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime)}
-        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime)}
-        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime)}
-        if Int.random(in: 0...12) > 12 { slots[4].show(hideTime: popupTime)}
-        
-        
-        let minDelay = popupTime / 2.0
-        let maxDelay = popupTime * 2.0
-        let delay = Double.random(in: minDelay...maxDelay)
-        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
-            self?.createEnemy()
+        func createSlot(at position: CGPoint) {
+            let slot = WhackSlot()
+            slot.configure(at: position)
+            addChild(slot)
+            slots.append(slot)
         }
+        
+        func createEnemy ( ) {
+            numRounds += 1
+            
+            if numRounds >= 30 {
+                for slot in slots {
+                    slot.hide()
+                }
+                let gameOver = SKSpriteNode(imageNamed: "gameOver")
+                gameOver.position = CGPoint(x: 512, y: 384)
+                gameOver.zPosition = 1
+                addChild(gameOver)
+                return
+            }
+            
+            
+            popupTime *= 0.991
+            
+            slots.shuffle()
+            slots[0].show(hideTime: popupTime)
+            
+            if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime)}
+            if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime)}
+            if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime)}
+            if Int.random(in: 0...12) > 12 { slots[4].show(hideTime: popupTime)}
+            
+            
+            let minDelay = popupTime / 2.0
+            let maxDelay = popupTime * 2.0
+            let delay = Double.random(in: minDelay...maxDelay)
+            DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+                self?.createEnemy()
+            }
+        }
+        
     }
-   
-}
