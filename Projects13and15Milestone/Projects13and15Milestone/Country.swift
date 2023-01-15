22dccbb3566d8bec5842f124c705baa53b4eece5
KarVarr <megatr9n@gmail.com>
2023-01-15 15:06:01 +0300

    added table and JSON

---
+++ Projects13and15Milestone/Projects13and15Milestone/Country.swift
@@ -0,0 +1,13 @@
+//
+//  Country.swift
+//  Projects13and15Milestone
+//
+//  Created by Karen Vardanian on 15.01.2023.
+//
+
+import Foundation
+
+
+struct County {
+    sd
+}
---
+++ Projects13and15Milestone/Projects13and15Milestone/Countrys.swift
@@ -0,0 +1,10 @@
+//
+//  Countrys.swift
+//  Projects13and15Milestone
+//
+//  Created by Karen Vardanian on 15.01.2023.
+//
+
+import Foundation
+
+
