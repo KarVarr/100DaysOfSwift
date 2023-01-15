9a15258f3b350e6d5a69ada7ea15e6846e948aee
KarVarr <megatr9n@gmail.com>
2023-01-15 14:53:59 +0300

    added project13and15Milestone

---
+++ Projects13and15Milestone/Projects13and15Milestone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Projects13and15Milestone
+//
+//  Created by Karen Vardanian on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
