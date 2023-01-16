83e6b11e07b4115a08f2b8a3fdc6f2a245c8c3d5
KarVarr <megatr9n@gmail.com>
2023-01-16 16:13:11 +0300

    added tableViewCell for milestone13-15 and some image for testing

---
+++ Projects13and15Milestone/Projects13and15Milestone/CustomTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CustomTableViewCell.swift
+//  Projects13and15Milestone
+//
+//  Created by Karen Vardanian on 16.01.2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+  
+    @IBOutlet weak var imageOfLabel: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
