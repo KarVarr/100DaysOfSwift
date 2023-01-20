21a5071b67e3e73fce5cd39b803d169e529084c2
KarVarr <megatr9n@gmail.com>
2023-01-20 16:33:31 +0300

    added project 18 debugging

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Karen Vardanian on 20.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
