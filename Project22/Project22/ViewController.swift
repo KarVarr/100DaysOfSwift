b46baf47fc4b72c57da8d19f10089bec731695d5
KarVarr <megatr9n@gmail.com>
2023-01-23 00:01:02 +0300

    added project 22 Core location

---
+++ Project22/Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Karen Vardanian on 23.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
