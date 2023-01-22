30fd65de46884112d7aa7fc888999429617e691a
KarVarr <megatr9n@gmail.com>
2023-01-23 00:16:24 +0300

    added CoreLOcation, CLLocationManager

--- Project22/Project22/ViewController.swift
+++ Project22/Project22/ViewController.swift
@@ -5,13 +5,32 @@
 //  Created by Karen Vardanian on 23.01.2023.
 //
 
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var distanceReading: UILabel!
+    var locationManager: CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    
+                }
+            }
+        }
     }
 
 
