135e5bef7aff6d70d7a6b6fae9540c00a708b430
KarVarr <megatr9n@gmail.com>
2023-01-29 11:39:41 +0300

    added NSMutableAttrivutedString

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -29,3 +29,21 @@ languages.contains("Swift")
 input.contains(languages[2])
 
 languages.contains(where: input.contains)
+
+
+let string = "This is a test string"
+//let attributes: [NSAttributedString.Key: Any] = [
+//    .foregroundColor: UIColor.white,
+//    .backgroundColor: UIColor.red,
+//    .font: UIFont.boldSystemFont(ofSize: 36)
+//]
+//
+//let attributedString = NSAttributedString(string: string, attributes: attributes)
+
+
+let attributedString = NSMutableAttributedString(string: string)
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 8), range: NSRange(location: 0, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 16), range: NSRange(location: 5, length: 2))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 24), range: NSRange(location: 8, length: 1))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 32), range: NSRange(location: 10, length: 4))
+attributedString.addAttribute(.font, value: UIFont.systemFont(ofSize: 40), range: NSRange(location: 15, length: 6))
