51713fc98f077d145909cd5020fb5a57327d1594
KarVarr <megatr9n@gmail.com>
2023-01-29 11:20:07 +0300

    added extension String

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+let name = "Taylor"
+
+for i in name {
+    print(i)
+}
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+let letter2 = name[3]
