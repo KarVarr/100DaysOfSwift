23e07423e0f35ec01be38fe7d26396491c3fabe0
KarVarr <megatr9n@gmail.com>
2023-01-29 11:31:43 +0300

    added contains

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -14,3 +14,18 @@ extension String {
 }
 
 let letter2 = name[3]
+
+let weather = "in's going to rain"
+weather.capitalized
+
+
+let input = "Swift is like Objective-C without the C"
+
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+input.contains(languages[2])
+
+languages.contains(where: input.contains)
