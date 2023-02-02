d01a5cc3077ff2c78e525553b1f34a26a058af3c
KarVarr <megatr9n@gmail.com>
2023-02-02 18:25:42 +0300

    added project 26

---
+++ Project26/Project26/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project26
+//
+//  Created by Karen Vardanian on 02.02.2023.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+  
+    override func didMove(to view: SKView) {
+        
+    }
+}
