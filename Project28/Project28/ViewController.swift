377c63dfb6d1e22ca4b7bc4937eedfd6a2e9f8bd
KarVarr <megatr9n@gmail.com>
2023-02-08 18:22:35 +0300

    added touch id face id

--- Project28/Project28/ViewController.swift
+++ Project28/Project28/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Karen Vardanian on 08.02.2023.
 //
-
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -24,7 +24,30 @@ class ViewController: UIViewController {
     }
     
     @IBAction func authenticateTapped(_ sender: Any) {
-        unlockSecretMessage()
+        let context = LAContext()
+        var error: NSError?
+
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [weak self] success, authenticationError in
+
+                DispatchQueue.main.async {
+                    if success {
+                        self?.unlockSecretMessage()
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "You could not be verified; please try again.", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unavailable", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(ac, animated: true)
+        }
     }
     
     @objc func adjustForKeyboard(notification: Notification) {
