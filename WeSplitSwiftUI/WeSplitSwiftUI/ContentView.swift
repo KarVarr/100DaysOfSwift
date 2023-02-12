6e45e3cbb3ad60d75c05a60efc5c8e5c633c67d5
KarVarr <megatr9n@gmail.com>
2023-02-13 00:09:08 +0300

    navigationView and Title

--- WeSplitSwiftUI/WeSplitSwiftUI/ContentView.swift
+++ WeSplitSwiftUI/WeSplitSwiftUI/ContentView.swift
@@ -9,21 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello, world!")
-            
+        NavigationView {
+            Form {
+                Section{
+                    Text("Hello, world!")
+                }
             }
-            Group {
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-                Text("Hello, world!")
-            }
-            
+            .navigationTitle("SwiftUI")
+            .navigationBarTitleDisplayMode(.inline)
         }
+        
+       
+       
     }
 }
 
