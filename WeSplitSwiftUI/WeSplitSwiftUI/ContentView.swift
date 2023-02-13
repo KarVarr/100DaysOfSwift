e5a2c9b400e2cc77d73a0668ff3468ae59531f89
KarVarr <megatr9n@gmail.com>
2023-02-13 12:53:39 +0300

    added focused() and toolbar

--- WeSplitSwiftUI/WeSplitSwiftUI/ContentView.swift
+++ WeSplitSwiftUI/WeSplitSwiftUI/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
@@ -30,6 +31,7 @@ struct ContentView: View {
                     TextField("Amount", value: $checkAmount, format:
                             .currency(code: "USD" ))
                             .keyboardType(.decimalPad)
+                            .focused($amountIsFocused)
                     Picker("Number Of People", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0)")
@@ -60,6 +62,14 @@ struct ContentView: View {
             .scrollContentBackground(.hidden)
             .background(Color.indigo)
             .navigationTitle("WeSplit")
+            .toolbar{
+                ToolbarItemGroup(placement: .keyboard, content: {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                })
+            }
             
             
         }
