aab1c0e3e0a48feb83edc9d3c8ec12f1c6b0ecab
KarVarr <megatr9n@gmail.com>
2023-02-14 19:35:40 +0300

    delete folder and create new for SwiftUI

---
+++ SwiftUIGuessTheFlag/SwiftUIGuessTheFlag/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIGuessTheFlag
+//
+//  Created by Karen Vardanian on 14.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIGuessTheFlag/SwiftUIGuessTheFlag/SwiftUIGuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGuessTheFlagApp.swift
+//  SwiftUIGuessTheFlag
+//
+//  Created by Karen Vardanian on 14.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
