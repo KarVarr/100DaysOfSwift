dcf71b6fe97c736f63d7d85e279bc217f1043e48
KarVarr <megatr9n@gmail.com>
2023-02-16 17:25:20 +0300

    added new project SwiftUIViewsAndModifiers

---
+++ SwiftUIViewsAndModifiers/SwiftUIViewsAndModifiers/SwiftUIViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewsAndModifiersApp.swift
+//  SwiftUIViewsAndModifiers
+//
+//  Created by Karen Vardanian on 16.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
