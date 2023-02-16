efa6a577fc6212f9994c0b07f800b7c142cde9f1
KarVarr <megatr9n@gmail.com>
2023-02-16 20:08:40 +0300

    added Challenge 3

--- SwiftUIViewsAndModifiers/SwiftUIViewsAndModifiers/ContentView.swift
+++ SwiftUIViewsAndModifiers/SwiftUIViewsAndModifiers/ContentView.swift
@@ -7,15 +7,32 @@
 
 import SwiftUI
 
+struct Title: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(.blue)
+            .font(.largeTitle)
+            .frame(width: 250, height: 80)
+            .background(.mint)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.6), radius: 5, x: 4, y: 4)
+    }
+}
+
+extension View {
+    func largeTitle() -> some View {
+        modifier(Title())
+    }
+}
+
+
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           Text("heheheheh")
+                .largeTitle()
         }
-        .padding()
     }
 }
 
