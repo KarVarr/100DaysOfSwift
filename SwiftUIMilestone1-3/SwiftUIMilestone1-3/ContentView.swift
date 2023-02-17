5e34f9223cc04cbb8a380bab6e56d8834ff4839d
KarVarr <megatr9n@gmail.com>
2023-02-17 17:21:10 +0300

    added a two picker and some style

--- SwiftUIMilestone1-3/SwiftUIMilestone1-3/ContentView.swift
+++ SwiftUIMilestone1-3/SwiftUIMilestone1-3/ContentView.swift
@@ -8,17 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickRandom = "Rock"
-    
-    
+    @State private var pickRandom = "Paper"
+    @State private var winOrLose = "Win"
+    let itemsForPick = ["Rock","Paper", "Scissors"]
+    let itemsForWinOrLoose = ["Win", "Lose"]
     
     var body: some View {
         VStack {
-            VStack {
+            VStack (spacing: 30){
                 Picker("Pick the peace", selection: $pickRandom) {
+                    ForEach(itemsForPick, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+                
+                Picker("Win Or Lose", selection: $winOrLose) {
+                    ForEach(itemsForWinOrLoose, id: \.self) {
+                        Text($0)
+                    }
                     
                 }
                 .pickerStyle(.segmented)
+                .padding(.horizontal)
+                
+            }
+            
+            .padding()
+            .background(.mint)
+            .clipShape(RoundedRectangle(cornerRadius: 10 ))
+            .padding(.horizontal)
+            
+            VStack {
+                Text("heel")
             }
         }
         
