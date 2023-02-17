a3d180780b8da5c7c2645c032e17c542fe0a189e
KarVarr <megatr9n@gmail.com>
2023-02-17 18:45:50 +0300

    added button and some logic

--- SwiftUIMilestone1-3/SwiftUIMilestone1-3/ContentView.swift
+++ SwiftUIMilestone1-3/SwiftUIMilestone1-3/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var pickRandom = "Paper"
+    @State private var userAnswer = "Rock"
     @State private var winOrLose = "Win"
+    
+    @State private var userScore = 0
     let itemsForPick = ["Rock","Paper", "Scissors"]
     let itemsForWinOrLoose = ["Win", "Lose"]
     
     var body: some View {
         VStack {
+            Text("Rock Paper Scissors")
+                .font(.largeTitle)
             VStack (spacing: 30){
+                Text("AI move!")
                 Picker("Pick the peace", selection: $pickRandom) {
                     ForEach(itemsForPick, id: \.self) {
                         Text($0)
@@ -34,18 +40,68 @@ struct ContentView: View {
                 .padding(.horizontal)
                 
             }
-            
             .padding()
             .background(.mint)
             .clipShape(RoundedRectangle(cornerRadius: 10 ))
+            .padding(.top, 10.0)
             .padding(.horizontal)
+            Spacer()
+            
             
             VStack {
-                Text("heel")
+                Text("Tap your move!")
+                Picker("User Answer", selection: $userAnswer) {
+                    ForEach(itemsForPick, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding()
+                
+                Button("Press") {
+                    buttonActionPress()
+                }
+                .frame(width: 150)
+                .padding()
+                .background(.cyan)
+                .foregroundColor(.black)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                
+                Spacer()
+                
+                Text("Your Score is : \(userScore)")
+                    .font(.largeTitle)
+                    .padding(.bottom, 20)
             }
+            .padding(.top, 50)
+            .background(.orange)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .padding()
+            Spacer()
         }
-        
     }
+    
+    func buttonActionPress () {
+        if pickRandom == "Rock" && userAnswer == "Paper" && winOrLose == "Win" ||
+            pickRandom == "Paper" && userAnswer == "Scissors" && winOrLose == "Win" ||
+            pickRandom == "Scissors" && userAnswer == "Rock" && winOrLose == "Win" ||
+            pickRandom == "Rock" && userAnswer == "Scissors" && winOrLose == "Lose" ||
+            pickRandom == "Paper" && userAnswer == "Rock" && winOrLose == "Lose" ||
+            pickRandom == "Scissors" && userAnswer == "Paper" && winOrLose == "Lose"
+        {
+            userScore += 1
+            pickRandom = itemsForPick.randomElement()!
+            winOrLose = itemsForWinOrLoose.randomElement()!
+            
+        } else {
+            userScore -= 1
+            pickRandom = itemsForPick.randomElement()!
+            winOrLose = itemsForWinOrLoose.randomElement()!
+        }
+    }
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
