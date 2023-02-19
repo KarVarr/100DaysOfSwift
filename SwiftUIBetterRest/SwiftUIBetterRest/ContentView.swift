759cd39f9184acb24a649f1537ea6a67f3c7ed83
KarVarr <megatr9n@gmail.com>
2023-02-19 16:51:41 +0300

    added BetterRest project 4 from SwiftUI

---
+++ SwiftUIBetterRest/SwiftUIBetterRest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIBetterRest
+//
+//  Created by Karen Vardanian on 19.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIBetterRest/SwiftUIBetterRest/SwiftUIBetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBetterRestApp.swift
+//  SwiftUIBetterRest
+//
+//  Created by Karen Vardanian on 19.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
