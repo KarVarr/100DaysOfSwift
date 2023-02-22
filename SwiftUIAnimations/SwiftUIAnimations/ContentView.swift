b6daac013f01e6de4b5847c3e56b79f9e3a36cc7
KarVarr <megatr9n@gmail.com>
2023-02-22 18:14:46 +0300

    added AnyTranstion and ViewModifier for pivot

--- SwiftUIAnimations/SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/ContentView.swift
@@ -6,23 +6,46 @@
 //
 
 import SwiftUI
+struct CornerRotateModifier: ViewModifier {
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(amount: 0, anchor: .topLeading)
+        )
+    }
+}
+
 
 struct ContentView: View {
-    @State private var showingFrame = false
+    @State private var isShowingRed = false
     
     var body: some View {
-        VStack {
-            Button("Tap") {
-                withAnimation {
-                    showingFrame.toggle()
-                }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
+            
+            if isShowingRed {
+                Rectangle()
+                    .fill(.mint)
+                    .frame(width: 200,height: 200)
+                    .transition(.pivot)
             }
-            if showingFrame {
-                Rectangle ()
-                    .fill(.indigo)
-                    .frame(width: 300, height: 300)
-                    //.transition(.scale)
-                    .transition(.asymmetric(insertion: .scale, removal: .scale))
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
             }
         }
     }
