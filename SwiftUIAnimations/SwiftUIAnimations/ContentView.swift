3dca6cb7387edc4214a38bec55dd4b5e1d4391f2
KarVarr <megatr9n@gmail.com>
2023-02-22 16:59:32 +0300

    added letters animation with dragGesture

--- SwiftUIAnimations/SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/SwiftUIAnimations/ContentView.swift
@@ -8,24 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    let letters = Array("Hello, SwiftUI")
+    @State private var enabled = false
     @State private var dragAmount = CGSize.zero
     
     var body: some View {
-        LinearGradient(gradient: Gradient(colors: [.black, .green]), startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            .shadow(color: .gray, radius: 8 , x: 4, y: 4)
-            .offset(dragAmount)
-            .gesture(
-                DragGesture()
-                    .onChanged{dragAmount = $0.translation}
+        HStack (spacing: 0){
+            ForEach(0..<letters.count, id: \.self) { num in
+                Text(String(letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .indigo : .mint)
+                    .offset(dragAmount)
+                    .animation(.default.delay(Double(num) / 20), value: dragAmount)
                 
-                    .onEnded{_ in
-                        withAnimation {
-                            dragAmount = .zero
-                        }
-                    }
-            )
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation}
+                .onEnded{ _ in
+                    dragAmount = .zero
+                    enabled.toggle()
+                }
+        )
         
     }
 }
