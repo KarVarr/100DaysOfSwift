d756d147c10ad44ad408fa2221906b496bf8b63b
KarVarr <megatr9n@gmail.com>
2023-02-23 13:46:26 +0300

    added project milestone 4-6

---
+++ SwiftUIMilestone4-6/SwiftUIMilestone4-6/SwiftUIMilestone4_6App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMilestone4_6App.swift
+//  SwiftUIMilestone4-6
+//
+//  Created by Karen Vardanian on 23.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMilestone4_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
