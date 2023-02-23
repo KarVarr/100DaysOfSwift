cbc6f0247ba1eaacaf5cd46e5ffa6f3f0f86a9e3
KarVarr <megatr9n@gmail.com>
2023-02-23 15:40:50 +0300

    added some buttons and text

--- SwiftUIMilestone4-6/SwiftUIMilestone4-6/ContentView.swift
+++ SwiftUIMilestone4-6/SwiftUIMilestone4-6/ContentView.swift
@@ -10,33 +10,116 @@ import SwiftUI
 struct ContentView: View {
     @State private var multiplicationTable = 1
     @State private var questions = 5
+    @State private var userAnswer = ""
     
     @State private var questionsArray = [5,10,20]
+    @State private var buttons = [1,2,3,4,5,6,7,8,9]
     
     var body: some View {
         NavigationView {
-            VStack {
-                Form {
-                    Section {
-                        Stepper("Choose a multiplication number: \(multiplicationTable)", value: $multiplicationTable, in: 1...9)
+            ZStack {
+                Color.gray
+                    .ignoresSafeArea()
+                VStack {
+                    VStack {
+                        
+                        Form {
+                            Section {
+                                Stepper("\(multiplicationTable)", value: $multiplicationTable, in: 1...9)
+                                    .font(.largeTitle)
+                                    .foregroundColor(.brown)
+                            } header: {
+                                Text("Choose a multiplication number: ")
+                            }
+                            Section {
+                                Picker("How many questions?", selection: $questions) {
+                                    ForEach (questionsArray, id: \.self) {
+                                        Text("\($0)")
+                                    }
+                                }
+                                .pickerStyle(.navigationLink)
+                                .font(.title)
+                                .foregroundColor(.brown)
+                            }
+                        }
+                        .scrollContentBackground(.hidden)
                     }
-                    Section {
-                        Picker("How many questions?", selection: $questions) {
-                            ForEach (questionsArray, id: \.self) {
-                                Text("\($0)")
+                    
+                    VStack(alignment: .center) {
+                        
+                        VStack(alignment: .center) {
+                            Text("What is  \(multiplicationTable) * 5 ")
+                            TextField("Write your answer", text: $userAnswer)
+                                .frame(width: 300)
+                                .border(.red)
+                                .padding(.bottom, 30)
+                                .keyboardType(.decimalPad)
+                        }
+                        .font(.largeTitle)
+                        
+                        
+                        VStack {
+                            ForEach(0..<3) {_ in
+                                HStack {
+                                    ForEach(0..<3) {_ in
+                                        ZStack {
+                                            RoundedRectangle(cornerRadius: 30)
+                                                .fill(.green)
+                                                .frame(width: 100,height: 50)
+                                            ForEach (0..<10) { num in
+                                                Button ("1") {}
+                                                   
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            HStack {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .fill(.green)
+                                        .frame(width: 100,height: 50)
+                                    
+                                    Button ("del") {}
+                                        .foregroundColor(.white)
+                                    
+                                }
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .fill(.green)
+                                        .frame(width: 100,height: 50)
+                                    
+                                    Button ("0") {}
+                                        .foregroundColor(.white)
+                                    
+                                }
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 30)
+                                        .fill(.green)
+                                        .frame(width: 100,height: 50)
+                                    
+                                    Button ("Submit") {}
+                                        .foregroundColor(.white)
+                                    
+                                }
+                                
                             }
                         }
                     }
-                    
                 }
                 
+               
                 
                 
             }
             //MARK: - NavigationTitle
             .navigationTitle("Multiplication")
+            .foregroundColor(.yellow)
+           
         }
+        
     }
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
