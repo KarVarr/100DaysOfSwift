244f97e20ff20b003f4ff6abb71a3b8d13fde017
KarVarr <megatr9n@gmail.com>
2023-02-23 14:15:54 +0300

    added stepper and picker

--- SwiftUIMilestone4-6/SwiftUIMilestone4-6/ContentView.swift
+++ SwiftUIMilestone4-6/SwiftUIMilestone4-6/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var multiplicationTable = 1
+    @State private var questions = 5
+    
+    @State private var questionsArray = [5,10,20]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Form {
+                    Section {
+                        Stepper("Choose a multiplication number: \(multiplicationTable)", value: $multiplicationTable, in: 1...9)
+                    }
+                    Section {
+                        Picker("How many questions?", selection: $questions) {
+                            ForEach (questionsArray, id: \.self) {
+                                Text("\($0)")
+                            }
+                        }
+                    }
+                    
+                }
+                
+                
+                
+            }
+            //MARK: - NavigationTitle
+            .navigationTitle("Multiplication")
         }
-        .padding()
     }
 }
 
