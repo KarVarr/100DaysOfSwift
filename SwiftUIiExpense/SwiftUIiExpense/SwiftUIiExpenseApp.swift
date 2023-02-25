9ead22b093d254758a1b585a4522becd32f5e6f9
KarVarr <megatr9n@gmail.com>
2023-02-25 18:50:48 +0300

    added new project iExpense

---
+++ SwiftUIiExpense/SwiftUIiExpense/SwiftUIiExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIiExpenseApp.swift
+//  SwiftUIiExpense
+//
+//  Created by Karen Vardanian on 25.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIiExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
