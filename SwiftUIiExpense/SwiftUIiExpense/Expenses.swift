b13513c49c7a2dc7d139d6d5e0a93eecbb4a63c2
KarVarr <megatr9n@gmail.com>
2023-02-26 22:16:38 +0300

    added UserDefauld and Codable

--- SwiftUIiExpense/SwiftUIiExpense/Expenses.swift
+++ SwiftUIiExpense/SwiftUIiExpense/Expenses.swift
@@ -8,5 +8,21 @@
 import Foundation
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
 }
