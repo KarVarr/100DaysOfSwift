c98cae2419513a5e38d66819028604acbde3428f
KarVarr <megatr9n@gmail.com>
2023-02-26 13:31:51 +0300

    added dismiss button and toolbar button

--- SwiftUIiExpense/SwiftUIiExpense/AddView.swift
+++ SwiftUIiExpense/SwiftUIiExpense/AddView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct AddView: View {
     @ObservedObject var expenses: Expenses
-    
+    @Environment(\.dismiss) var dismiss
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
@@ -31,6 +31,16 @@ struct AddView: View {
                 
                 TextField("Amount", value: $amount, format: .currency(code: "USD"))
                     .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                } label: {
+                    Image(systemName: "pencil.and.outline")
+                }
             }
         }
     }
