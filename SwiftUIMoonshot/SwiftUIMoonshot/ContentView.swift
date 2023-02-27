f9a62edc65345b4fe21091309f3d3c34289f0bbb
KarVarr <megatr9n@gmail.com>
2023-02-27 17:09:41 +0300

    added date formatted

--- SwiftUIMoonshot/SwiftUIMoonshot/ContentView.swift
+++ SwiftUIMoonshot/SwiftUIMoonshot/ContentView.swift
@@ -11,8 +11,42 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        Text("\(astronauts.count)")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text(mission.description)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                            .padding(5)
+                            .background(.mint)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                        .padding(20)
+                        .shadow(color: .gray, radius: 10)
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
        
     }
 }
