4ec4b82b3e6b59d3109a0c8e95d4e1dc8e84be04
KarVarr <megatr9n@gmail.com>
2023-02-27 20:06:53 +0300

    added AstronautView

---
+++ SwiftUIMoonshot/SwiftUIMoonshot/AstronautView.swift
@@ -0,0 +1,35 @@
+//
+//  AstronautView.swift
+//  SwiftUIMoonshot
+//
+//  Created by Karen Vardanian on 27.02.2023.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .navigationTitle(astronaut.name)
+        .background(.darkBackground)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["aldrin"]!)
+            .preferredColorScheme(.dark)
+    }
+}
