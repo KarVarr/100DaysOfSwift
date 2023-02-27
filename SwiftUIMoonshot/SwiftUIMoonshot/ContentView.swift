8c6278c25584b184944c15a7ccd26dc9b0a450f6
KarVarr <megatr9n@gmail.com>
2023-02-27 14:51:54 +0300

    added ScrollView and lazyVStack

--- SwiftUIMoonshot/SwiftUIMoonshot/ContentView.swift
+++ SwiftUIMoonshot/SwiftUIMoonshot/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("Example")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.9)
-                .frame(width: geo.size.width, height: geo.size.height)
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 20) {
+                ForEach(0..<100) {
+                    Text("Hello \($0)")
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
        
     }
