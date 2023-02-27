6885a39662e87ed4d2cec07c1deabd21c2a799e2
KarVarr <megatr9n@gmail.com>
2023-02-27 14:04:59 +0300

    added the new project Moonshot

---
+++ SwiftUIMoonshot/SwiftUIMoonshot/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIMoonshot
+//
+//  Created by Karen Vardanian on 27.02.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIMoonshot/SwiftUIMoonshot/SwiftUIMoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMoonshotApp.swift
+//  SwiftUIMoonshot
+//
+//  Created by Karen Vardanian on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
