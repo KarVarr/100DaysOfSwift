ab6824e3de31ad0b205652de875197d30e58ed4c
KarVarr <megatr9n@gmail.com>
2023-02-28 19:54:48 +0300

    added InsettableShape

--- SwiftUIDrawing/SwiftUIDrawing/ContentView.swift
+++ SwiftUIDrawing/SwiftUIDrawing/ContentView.swift
@@ -19,21 +19,28 @@ struct Triangle: Shape {
     }
 }
 
-struct Arc: Shape {
-        let startAngle: Angle
-        let endAngle: Angle
-        let clockwise: Bool
-        
+struct Arc: InsettableShape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    var insetAmount = 0.0
+    
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
         let modifiedStart = startAngle - rotationAdjustment
         let modifiedEnd = endAngle - rotationAdjustment
         
-            var path = Path()
+        var path = Path()
         
-        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
         
         return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
     }
 }
 
@@ -42,10 +49,14 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+                .strokeBorder(.indigo, lineWidth: 40)
+            
             Arc(startAngle: .degrees(0), endAngle: .degrees(Double(180)), clockwise: true)
                 .stroke(style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
                 .fill(.red)
                 .padding()
+                .frame(width: 50, height: 50)
             
             
             Triangle()
@@ -53,10 +64,10 @@ struct ContentView: View {
                 .fill(.mint)
             
                 .frame(width: 250, height: 250)
-           
+            
         }
-
-
+        
+        
     }
 }
 
