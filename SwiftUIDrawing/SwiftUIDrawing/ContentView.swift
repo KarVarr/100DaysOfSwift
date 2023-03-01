3d5d3e9a770c27375d0a1831349c2c1b5f79196e
KarVarr <megatr9n@gmail.com>
2023-03-01 19:08:43 +0300

    added //Challenge 1

--- SwiftUIDrawing/SwiftUIDrawing/ContentView.swift
+++ SwiftUIDrawing/SwiftUIDrawing/ContentView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+//Challenge 1
 struct Rectangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -20,7 +20,7 @@ struct Rectangle: Shape {
     }
 }
 
-
+//Challenge 1
 struct Triangle: Shape {
     func path(in rect: CGRect) -> Path {
         var path = Path()
@@ -28,21 +28,30 @@ struct Triangle: Shape {
         path.addLine(to: CGPoint(x: 300, y: 350))
         path.addLine(to: CGPoint(x: 300, y: 350))
         path.addLine(to: CGPoint(x: 150, y: 100))
+        path.addLine(to: CGPoint(x: 0, y: 350))
         return path
     }
 }
 
 struct ContentView: View {
-    @State private var insertAmount = 50.0
+    @State private var lineThickness = 10.0
     
     var body: some View {
         VStack {
+            //Challenge 1
             Triangle()
+                .stroke(.mint, style: StrokeStyle(lineWidth: lineThickness , lineCap: .round, lineJoin: .round))
+               
+            
                 .frame(width: 300, height: 300)
-                
+            //Challenge 1
             Rectangle()
+                .stroke(.mint, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
                 .frame(width: 100, height: 300)
+            //Challenge 2
+            Slider(value: $lineThickness, in: 3...10)
         }
+        
     }
 }
 
