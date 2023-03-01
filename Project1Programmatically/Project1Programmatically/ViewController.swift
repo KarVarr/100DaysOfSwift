c7cf9916bcf8f18a5c6fff05482fdc17b7cf19eb
KarVarr <megatr9n@gmail.com>
2023-03-01 21:25:49 +0300

    added project1 Programmatically

---
+++ Project1Programmatically/Project1Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1Programmatically
+//
+//  Created by Karen Vardanian on 01.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
