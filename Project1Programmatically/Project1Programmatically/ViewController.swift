0d599746972486a0f790574b354265377265cc97
KarVarr <megatr9n@gmail.com>
2023-03-01 21:38:12 +0300

    ADDED pictures into project 1 and  the fileManage

--- Project1Programmatically/Project1Programmatically/ViewController.swift
+++ Project1Programmatically/Project1Programmatically/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .cyan
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+       
     }
 
 
