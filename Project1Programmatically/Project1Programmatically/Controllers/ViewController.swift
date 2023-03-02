cb84d032a88e974f88276ce20c87876d34971f5c
KarVarr <megatr9n@gmail.com>
2023-03-02 20:41:23 +0300

    added UITableView into ViewController

--- Project1Programmatically/Project1Programmatically/Controllers/ViewController.swift
+++ Project1Programmatically/Project1Programmatically/Controllers/ViewController.swift
@@ -8,17 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     var pictures = [String]()
+    
+    let tableView = UITableView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.5960784314, green: 0.8666666667, blue: 0.7921568627, alpha: 1)
-        title = "Main View"
-        navigationController?.navigationBar.titleTextAttributes = [
-            NSAttributedString.Key.foregroundColor: UIColor.black,
-            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24)
-        ]
+        settings()
+        addViews()
+        layout()
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
@@ -33,5 +31,50 @@ class ViewController: UIViewController {
     }
     
     
+    func settings () {
+        title = "Main View"
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.black,
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24)
+        ]
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = #colorLiteral(red: 0.5656551123, green: 0.8747014403, blue: 0.6658728719, alpha: 1)
+        tableView.allowsSelectionDuringEditing = true
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    func addViews() {
+        
+        
+        view.addSubview(tableView)
+    }
+    
+    func layout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    
 }
 
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        pictures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.imageView?.image = UIImage(named: pictures[indexPath.row])
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    
+}
