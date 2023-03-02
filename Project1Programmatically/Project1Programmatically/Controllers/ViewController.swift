f4deef15b625fb64878a5e0d786eba78b33cc645
KarVarr <megatr9n@gmail.com>
2023-03-02 19:36:48 +0300

    added navigation

--- Project1Programmatically/Project1Programmatically/Controllers/ViewController.swift
+++ Project1Programmatically/Project1Programmatically/Controllers/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        view.backgroundColor = #colorLiteral(red: 0.5960784314, green: 0.8666666667, blue: 0.7921568627, alpha: 1)
+        title = "Main View"
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.black,
+            NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 24)
+        ]
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
@@ -22,9 +29,9 @@ class ViewController: UIViewController {
                 pictures.append(item)
             }
         }
-       
+        
     }
-
-
+    
+    
 }
 
