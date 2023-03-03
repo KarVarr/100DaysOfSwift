ba75c0112021503f4720a3c541f840ca350cf953
KarVarr <megatr9n@gmail.com>
2023-03-03 20:06:15 +0300

    added SwiftUIMilestone7-9

---
+++ SwiftUIMilestone7-9/SwiftUIMilestone7-9/SwiftUIMilestone7_9App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMilestone7_9App.swift
+//  SwiftUIMilestone7-9
+//
+//  Created by Karen Vardanian on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMilestone7_9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
