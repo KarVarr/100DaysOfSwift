e1af68fa823a7c7dcb09a2cf5f2ab7ac2111bf64
KarVarr <megatr9n@gmail.com>
2023-03-06 17:12:11 +0300

    AsyncImage with placeholder and ProgressView

--- SwiftUICupcakeCorner/SwiftUICupcakeCorner/ContentView.swift
+++ SwiftUICupcakeCorner/SwiftUICupcakeCorner/ContentView.swift
@@ -8,47 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var results = [Result]()
-    
     var body: some View {
-        NavigationView {
-            List(results, id: \.trackId) { item in
-                NavigationLink(destination: DetailView(result: Result(trackId: item.trackId, trackName: item.trackName, collectionName: item.collectionName, artworkUrl30: item.artworkUrl30, artworkUrl100: item.artworkUrl100, country: item.country, trackTimeMillis: item.trackTimeMillis))) {
-                    VStack (alignment: .leading) {
-                        HStack {
-                            AsyncImage(url: URL(string: item.artworkUrl30)!)
-                                .scaledToFit()
-                                .frame(width: 30, height: 30)
-                                .clipShape(RoundedRectangle(cornerRadius: 10))
-                            Text(item.trackName)
-                                .font(.headline)
-                        }
-                        Text(item.collectionName)
-                        Text( String( format: "%.2f", Double(item.trackTimeMillis) / 60000.0) + " minuts")
-                    }
-                }
-            }.task {
-                await loadData()
+        AsyncImage(url: URL(string: "https://hws.dev/img/log.png")) {phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image")
+            } else {
+                ProgressView()
             }
-            .navigationTitle("Taylor Swift")
         }
+        .frame(width: 200, height: 200)
+        
     }
     
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("invalid URL")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("invalid data")
-        }
-    }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
