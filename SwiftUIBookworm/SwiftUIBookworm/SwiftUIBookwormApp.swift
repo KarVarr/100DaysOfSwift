7d16b1d4282b1a2eb147df30697b29bb5e6520f2
KarVarr <megatr9n@gmail.com>
2023-03-07 19:14:29 +0300

    added two projects, one for swiftUI, the other one for UIKit

---
+++ SwiftUIBookworm/SwiftUIBookworm/SwiftUIBookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBookwormApp.swift
+//  SwiftUIBookworm
+//
+//  Created by Karen Vardanian on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
