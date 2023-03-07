e39920168d7da3d2c46e553325dba3ea3f8e2506
KarVarr <megatr9n@gmail.com>
2023-03-07 20:47:56 +0300

    added three buttons

--- Project2Programmatically/Project2Programmatically/Controller/ViewController.swift
+++ Project2Programmatically/Project2Programmatically/Controller/ViewController.swift
@@ -9,28 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var pictures = [String]()
+    
     var buttonFlag1 = UIButton(type: .system)
-
+    var buttonFlag2 = UIButton(type: .system)
+    var buttonFlag3 = UIButton(type: .system)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .systemTeal
+        navigationItem.largeTitleDisplayMode = .always
+        navigationController?.navigationBar.prefersLargeTitles = true
         title = "Guess the flag"
-        addView()
-        settings()
-        layout()
         
+        
+        addView()
+        settings(to: buttonFlag1, and: "UK")
+        settings(to: buttonFlag2, and: "Germany")
+        settings(to: buttonFlag3, and: "US")
+        layout()
     }
     
+    //MARK: - Functions
     func addView() {
         view.addSubview(buttonFlag1)
+        view.addSubview(buttonFlag2)
+        view.addSubview(buttonFlag3)
     }
     
-    func settings() {
-        buttonFlag1.translatesAutoresizingMaskIntoConstraints = false
-        buttonFlag1.setTitle("button 1 ", for: .normal)
-        buttonFlag1.backgroundColor = .systemMint
-        buttonFlag1.layer.cornerRadius = 10
-        buttonFlag1.setImage(UIImage(named: "US"), for: .normal)
+    func settings(to button: UIButton, and name: String) {
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setBackgroundImage(UIImage(named: name), for: .normal)
+        button.layer.cornerRadius = 10
     }
     
     func layout() {
@@ -39,6 +49,16 @@ class ViewController: UIViewController {
             buttonFlag1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
             buttonFlag1.widthAnchor.constraint(equalToConstant: 200),
             buttonFlag1.heightAnchor.constraint(equalToConstant: 100),
+            
+            buttonFlag2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonFlag2.topAnchor.constraint(equalTo: buttonFlag1.bottomAnchor, constant: 20),
+            buttonFlag2.widthAnchor.constraint(equalToConstant: 200),
+            buttonFlag2.heightAnchor.constraint(equalToConstant: 100),
+            
+            buttonFlag3.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonFlag3.topAnchor.constraint(equalTo: buttonFlag2.bottomAnchor, constant: 20),
+            buttonFlag3.widthAnchor.constraint(equalToConstant: 200),
+            buttonFlag3.heightAnchor.constraint(equalToConstant: 100),
         ])
     }
 
