3664dfb5e33c3ef03d637a3159dc454e9d033aa6
KarVarr <megatr9n@gmail.com>
2023-03-07 22:51:32 +0300

    added alert

--- Project2Programmatically/Project2Programmatically/Controller/ViewController.swift
+++ Project2Programmatically/Project2Programmatically/Controller/ViewController.swift
@@ -9,35 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var countries = [String]()
+    var allCountries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var countries: [String]!
     var score = 0
     var correctAnswer = 0
     
-    var buttonFlag1 = UIButton(type: .system)
-    var buttonFlag2 = UIButton(type: .system)
-    var buttonFlag3 = UIButton(type: .system)
-    
+    var buttonFlag1 = UIButton()
+    var buttonFlag2 = UIButton()
+    var buttonFlag3 = UIButton()
+   
+   
     var titleLabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        correctAnswer = Int.random(in: 0...2)
+        countries = allCountries.shuffled()
         
-       
         addView()
         allSettings()
-        settings(to: buttonFlag1)
-        settings(to: buttonFlag2)
-        settings(to: buttonFlag3)
-        askQuestion()
         layout()
+        askQuestion()
     }
     
     //MARK: - Functions
     func askQuestion () {
-        correctAnswer = Int.random(in: 0...2)
-        
+        settings(to: buttonFlag1, with: 0)
         buttonFlag1.setBackgroundImage(UIImage(named: countries[0]), for: .normal)
+        settings(to: buttonFlag2, with: 1)
         buttonFlag2.setBackgroundImage(UIImage(named: countries[1]), for: .normal)
+        settings(to: buttonFlag3, with: 2)
         buttonFlag3.setBackgroundImage(UIImage(named: countries[2]), for: .normal)
     }
     
@@ -49,7 +50,7 @@ class ViewController: UIViewController {
         view.addSubview(titleLabel)
     }
     
-    func settings(to button: UIButton) {
+    func settings(to button: UIButton, with tag: Int) {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.layer.borderWidth = 1
         button.layer.borderColor = UIColor.systemPink.cgColor
@@ -59,6 +60,10 @@ class ViewController: UIViewController {
         button.layer.shadowOpacity = 0.5
         button.layer.shadowRadius = 8
         button.layer.cornerRadius = 10
+        button.tag = tag
+        
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    
     }
     
     func allSettings() {
@@ -67,12 +72,10 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
         
         title = "Guess the flag"
-        
-        countries += ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-        
+    
         
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.text = "Choose the flag of: '\(countries[correctAnswer])' !"
+        titleLabel.text = "Choose the flag of \(countries[correctAnswer]) !"
         titleLabel.textColor = .systemTeal
         titleLabel.font = UIFont.systemFont(ofSize: 22)
     }
@@ -102,7 +105,30 @@ class ViewController: UIViewController {
             
         ])
     }
+    @objc func buttonTapped(_ sender: UIButton) {
+        var title: String
+        var message: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            message = "And this is the correct answer."
+            score += 1
+        } else {
+            title = "Wrong"
+            message = "This is the wrong answer"
+            score -= 1
+        }
+        print(sender.tag, correctAnswer)
+        print(countries!)
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+       
+      
+    }
 
+    
 
 }
 
