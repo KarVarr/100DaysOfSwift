8bd6dbdc5af82ff4c8e0ddcfe84c1ed8eb87639d
KarVarr <megatr9n@gmail.com>
2023-03-08 16:19:58 +0300

    added AddBookView

--- SwiftUIBookworm/SwiftUIBookworm/ContentView.swift
+++ SwiftUIBookworm/SwiftUIBookworm/ContentView.swift
@@ -11,28 +11,10 @@ import SwiftUI
 
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
-    
+   
     var body: some View {
         VStack {
-            List(students) { student in
-                Text(student.name ?? "Unknown")
-            }
-            Button("Add") {
-                let firstName = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
-                let lastName = ["Granger", "Lovegood", "Potter", "Weasley"]
-                
-                let chosenFirstName = firstName.randomElement() ?? "UnknownFirstName"
-                let chosenLastName = lastName.randomElement() ?? "UnknownLastName"
-                
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? moc.save()
-            }
-            
+       
         }
     }
 }
