b808404e5a8a0d688132c67b8277c1b988b70a03
KarVarr <megatr9n@gmail.com>
2023-03-09 20:45:21 +0300

    added milestore1-3 programmatically

---
+++ Milestone1-3Programmatically/Milestone1-3Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone1-3Programmatically
+//
+//  Created by Karen Vardanian on 09.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
