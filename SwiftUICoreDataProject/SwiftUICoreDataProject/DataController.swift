69af2505058124cc8053b7460a8991800dff26d7
KarVarr <megatr9n@gmail.com>
2023-03-10 21:14:15 +0300

    added DataController and DataCoreProject

---
+++ SwiftUICoreDataProject/SwiftUICoreDataProject/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  SwiftUICoreDataProject
+//
+//  Created by Karen Vardanian on 10.03.2023.
+//
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "CoreDataProject")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed, not load \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
