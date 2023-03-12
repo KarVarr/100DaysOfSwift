66707391d8cc97f274808d3bf34e06da56a312a1
KarVarr <megatr9n@gmail.com>
2023-03-12 11:56:04 +0300

    added LazyVGrid

--- SwiftUIMilestone10-12/SwiftUIMilestone10-12/ContentView.swift
+++ SwiftUIMilestone10-12/SwiftUIMilestone10-12/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var arr = ["a", "b", "c", "d", "e", "f", "g"]
+    
+    private var adaptiveColumns = [ GridItem(.adaptive(minimum: 170))]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns ,spacing: 30) {
+                    ForEach(arr, id: \.self) { item in
+                        VStack(alignment: .leading) {
+                            Text("name \(item)")
+                            Text("email \(item)")
+                        }
+                        .frame(minWidth: 170, maxWidth: 170, minHeight: 170, maxHeight: 170)
+                        .background(.red)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                    }
+                }
+            }
+            .navigationTitle("Friends 😛")
         }
-        .padding()
+       
     }
 }
 
