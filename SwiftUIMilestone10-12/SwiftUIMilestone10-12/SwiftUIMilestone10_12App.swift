17213ec442090f69fc12cfe4179109c740fcc2ac
KarVarr <megatr9n@gmail.com>
2023-03-12 11:40:47 +0300

    added new project SwiftUIMilestone10-12

---
+++ SwiftUIMilestone10-12/SwiftUIMilestone10-12/SwiftUIMilestone10_12App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMilestone10_12App.swift
+//  SwiftUIMilestone10-12
+//
+//  Created by Karen Vardanian on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMilestone10_12App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
