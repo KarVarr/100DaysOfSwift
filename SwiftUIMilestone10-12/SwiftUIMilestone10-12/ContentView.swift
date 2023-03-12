69e22828c9842e444fca2408de6d0c887c3fbeae
KarVarr <megatr9n@gmail.com>
2023-03-12 12:29:08 +0300

    added DetailView and Full size of Vstack

--- SwiftUIMilestone10-12/SwiftUIMilestone10-12/ContentView.swift
+++ SwiftUIMilestone10-12/SwiftUIMilestone10-12/ContentView.swift
@@ -16,14 +16,22 @@ struct ContentView: View {
             ScrollView {
                 LazyVGrid(columns: adaptiveColumns ,spacing: 30) {
                     ForEach(arr, id: \.self) { item in
-                        VStack(alignment: .leading) {
-                            Text("name \(item)")
-                            Text("email \(item)")
+                        NavigationLink(destination: DetailView()) {
+                            VStack(alignment: .leading) {
+                                Text("name \(item)")
+                                    .font(.system(size: 30, weight: .medium, design: .rounded))
+                                    .foregroundColor(.white)
+                                
+                                Text("email \(item)")
+                                    .font(.system(size: 24, weight: .light, design: .serif))
+                                    .foregroundColor(.gray)
+                            }
+                            .border(.black)
+                            .frame(minWidth: 170, minHeight: 170, alignment: .topLeading)
+                            .background(.mint)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            
                         }
-                        .frame(minWidth: 170, maxWidth: 170, minHeight: 170, maxHeight: 170)
-                        .background(.red)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                        
                     }
                 }
             }
---
+++ SwiftUIMilestone10-12/SwiftUIMilestone10-12/View/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  SwiftUIMilestone10-12
+//
+//  Created by Karen Vardanian on 12.03.2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .navigationTitle("Detail")
+            .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
