8885bd6d38616b9ae00962827c753d9d9fa4b338
KarVarr <megatr9n@gmail.com>
2023-03-13 21:07:41 +0300

    added project 5 prorgammatically

---
+++ Project5Programmatically/Project5Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5Programmatically
+//
+//  Created by Karen Vardanian on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
