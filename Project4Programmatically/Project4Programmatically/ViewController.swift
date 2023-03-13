13a4a4acbe49e0fa3410df2b674892ade0fc548c
KarVarr <megatr9n@gmail.com>
2023-03-13 16:25:44 +0300

    added alert and title didFinish

--- Project4Programmatically/Project4Programmatically/ViewController.swift
+++ Project4Programmatically/Project4Programmatically/ViewController.swift
@@ -13,19 +13,60 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
+       
         view = webView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "WEB KIT"
-        navigationController?.navigationBar.prefersLargeTitles = true
         
-        let url = URL(string: "https://www.apple.com")
+        addView()
+        setting()
+        layout()
+       
+        
+        let url = URL(string: "https://www.yandex.ru")
         webView.load(URLRequest(url: url!))
         webView.allowsBackForwardNavigationGestures = true
     }
-
+    
+    func addView() {
+        
+    }
+    
+    func setting() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    func layout() {
+        NSLayoutConstraint.activate([
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+      
+    }
+    
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else {return}
+        guard let url = URL(string: "https://\(actionTitle)") else {return}
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 
 }
 
