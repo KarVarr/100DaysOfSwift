ac298798f07d0666bd0d0e862dcb5bf7029554e7
KarVarr <megatr9n@gmail.com>
2023-03-15 21:00:16 +0300

    added Challenge 1,2,3

--- Project6bProgrammatically/Project6bProgrammatically/ViewController.swift
+++ Project6bProgrammatically/Project6bProgrammatically/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 class ViewController: UIViewController {
     
     let label1: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = .red
         label.text = "THERE"
+        label.textAlignment = .center
         label.sizeToFit()
         return label
     }()
     
     let label2: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = .cyan
         label.text = "ARE"
@@ -28,7 +29,7 @@ class ViewController: UIViewController {
     }()
     
     let label3: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = .yellow
         label.text = "SOME"
@@ -37,7 +38,7 @@ class ViewController: UIViewController {
     }()
     
     let label4: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = .green
         label.text = "AWESOME"
@@ -46,17 +47,19 @@ class ViewController: UIViewController {
     }()
     
     let label5: UILabel = {
-       let label = UILabel()
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.backgroundColor = .orange
         label.text = "LABELS"
         label.sizeToFit()
         return label
     }()
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .gray
+        view.backgroundColor = .systemTeal
         
         addView()
         layout()
@@ -71,11 +74,36 @@ class ViewController: UIViewController {
     }
     
     func layout() {
-        NSLayoutConstraint.activate([
-            
-        ])
-    }
-
-
+        //Challenge 1,2,3
+            NSLayoutConstraint.activate([
+                label1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+                label1.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                label1.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                label1.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1, constant: 2),
+                
+                label2.topAnchor.constraint(equalTo: label1.bottomAnchor, constant: 10),
+                label2.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                label2.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                label2.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1, constant: 2),
+                
+                label3.topAnchor.constraint(equalTo: label2.bottomAnchor, constant: 10),
+                label3.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                label3.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                label3.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1, constant: 2),
+                
+                label4.topAnchor.constraint(equalTo: label3.bottomAnchor, constant: 10),
+                label4.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                label4.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                label4.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1, constant: 2),
+                
+                label5.topAnchor.constraint(equalTo: label4.bottomAnchor, constant: 10),
+                label5.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+                label5.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+                label5.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1, constant: 2),
+            ])
+        }
+    
+    
+    
 }
 
