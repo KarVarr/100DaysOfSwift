83d0a6dcbe44fe97f1a1013809223296cf972375
KarVarr <megatr9n@gmail.com>
2023-03-16 19:35:25 +0300

    added Alert and addTextField(), submit button adding to the array input

--- Milestone4-6Programmatically/Milestone4-6Programmatically/Controller/TableViewController.swift
+++ Milestone4-6Programmatically/Milestone4-6Programmatically/Controller/TableViewController.swift
@@ -8,16 +8,22 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-    let arr = ["1","2","3"]
+    let list = ListOfItems()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigation()
         addView()
         settings()
         layout()
-
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+    }
+    
+    func navigation() {
+        title = "Shopping list"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewItem))
     }
     
     func addView() {
@@ -26,6 +32,7 @@ class TableViewController: UITableViewController {
     
     func settings() {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "reuseIdentifier")
+        
     }
     
     func layout() {
@@ -33,6 +40,27 @@ class TableViewController: UITableViewController {
     }
     
     
+    //MARK: - Functions
+    
+    @objc func addNewItem () {
+        let ac = UIAlertController(title: "New item", message: "Add a new item to the list", preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Add +", style: .default) {
+            [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else {return}
+            self?.submit(item)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        list.newItemArray.insert(item, at: 0)
+        
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+    }
     
     
     
@@ -40,16 +68,20 @@ class TableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arr.count
+        return list.newItemArray.count
     }
 
  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
+        cell.textLabel?.text = list.newItemArray[indexPath.row]
   
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 
 
---
+++ Milestone4-6Programmatically/Milestone4-6Programmatically/Model/ListOfItems.swift
@@ -0,0 +1,13 @@
+//
+//  ListOfItems.swift
+//  Milestone4-6Programmatically
+//
+//  Created by Karen Vardanian on 16.03.2023.
+//
+
+import Foundation
+
+
+class ListOfItems {
+    var newItemArray = [String]()
+}
