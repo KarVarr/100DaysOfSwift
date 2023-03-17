606d6f90a6debb1e8b65335e75cb8ac879b4d2a1
KarVarr <megatr9n@gmail.com>
2023-03-17 17:57:29 +0300

    added new Project SwiftUIInstafilter

---
+++ SwiftUIInstafilter/SwiftUIInstafilter/SwiftUIInstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIInstafilterApp.swift
+//  SwiftUIInstafilter
+//
+//  Created by Karen Vardanian on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIInstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
