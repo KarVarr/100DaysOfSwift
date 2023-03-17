10b305174df80d27317d2c4bd3fbf86ca6c57dd4
KarVarr <megatr9n@gmail.com>
2023-03-17 18:27:55 +0300

    added .confirmationDialog

--- SwiftUIInstafilter/SwiftUIInstafilter/ContentView.swift
+++ SwiftUIInstafilter/SwiftUIInstafilter/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingConfirmation = false
+    @State private var backgroundColor = Color.white
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .frame(width: 300, height: 300)
+                .background(backgroundColor)
+                .onTapGesture {
+                    showingConfirmation = true
+                }
+                .confirmationDialog("Change color", isPresented: $showingConfirmation) {
+                    Button("Red") {backgroundColor = .red}
+                    Button("Green") {backgroundColor = .green}
+                    Button("Mint") {backgroundColor = .mint}
+                    Button("Cancel", role: .cancel){}
+                } message: {
+                    Text("Change text color")
+                }
         }
         .padding()
     }
