58ed48a1a11bd2e844057e32476ec2a6fcdf21aa
KarVarr <megatr9n@gmail.com>
2023-03-19 15:11:21 +0300

    added new project SwiftUIBucketLIst

---
+++ SwiftUIBucketList/SwiftUIBucketList/SwiftUIBucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBucketListApp.swift
+//  SwiftUIBucketList
+//
+//  Created by Karen Vardanian on 19.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
