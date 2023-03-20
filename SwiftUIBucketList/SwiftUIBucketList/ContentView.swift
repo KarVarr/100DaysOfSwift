81d1081c5f8455368e8f4d994d453423383c67c8
KarVarr <megatr9n@gmail.com>
2023-03-20 15:10:07 +0300

    added improving Map annotations

--- SwiftUIBucketList/SwiftUIBucketList/ContentView.swift
+++ SwiftUIBucketList/SwiftUIBucketList/ContentView.swift
@@ -15,7 +15,18 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+                MapAnnotation(coordinate: location.coordinate) {
+                    VStack {
+                        Image(systemName: "star.circle")
+                            .resizable()
+                            .foregroundColor(.red)
+                            .frame(width: 44,height: 44)
+                            .background(.white)
+                            .clipShape(Circle())
+                        
+                        Text(location.name)
+                    }
+                }
             }
             .ignoresSafeArea()
 
--- SwiftUIBucketList/SwiftUIBucketList/Location.swift
+++ SwiftUIBucketList/SwiftUIBucketList/Location.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 struct Location: Identifiable, Codable, Equatable {
     let id: UUID
@@ -13,4 +14,14 @@ struct Location: Identifiable, Codable, Equatable {
     var description: String
     let latitude: Double
     let longitude: Double
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    static let example = Location(id: UUID(), name: "Buckingham Palace", description: "Where Queen lived", latitude: 51.501, longitude: -0.141)
+    
+    static func == (lhs: Location, rhs: Location) -> Bool {
+        lhs.id == rhs.id
+    }
 }
