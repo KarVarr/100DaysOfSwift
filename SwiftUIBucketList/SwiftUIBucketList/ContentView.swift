3dae77f47ed3671600b768f63b786d9cae54a797
KarVarr <megatr9n@gmail.com>
2023-03-20 14:21:34 +0300

    added FaceID with SwiftUI

--- SwiftUIBucketList/SwiftUIBucketList/ContentView.swift
+++ SwiftUIBucketList/SwiftUIBucketList/ContentView.swift
@@ -4,42 +4,43 @@
 //
 //  Created by Karen Vardanian on 19.03.2023.
 //
-import MapKit
 import SwiftUI
+import LocalAuthentication
 
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @State private var isUnlocked = false
     
     var body: some View {
-        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-            MapAnnotation(coordinate: location.coordinate) {
-                VStack {
-                    Image(systemName: "info.circle")
-                        .foregroundColor(.orange)
-                        .font(.system(size: 32))
-                    Text("\(location.name)")
-                        .foregroundColor(.white)
-                        .font(.headline)
-                        .padding(10)
-                        .background(.black.opacity(0.3))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-
-                }
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
         }
-            .ignoresSafeArea()
+        .onAppear(perform: authenticate)
     }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data 2"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isUnlocked = true
+                } else {
+                    
+                }
+            }
+        } else {
+            
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
