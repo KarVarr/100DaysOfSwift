0145113ebaf230b7f71d0572a8fdeba12ad0670b
KarVarr <megatr9n@gmail.com>
2023-03-22 18:33:39 +0300

    added SwiftUIMilestone13-15

---
+++ SwiftUIMilestone13-15/SwiftUIMilestone13-15/SwiftUIMilestone13_15App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMilestone13_15App.swift
+//  SwiftUIMilestone13-15
+//
+//  Created by Karen Vardanian on 22.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMilestone13_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
