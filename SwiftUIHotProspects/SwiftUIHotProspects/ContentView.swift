640bd4eb075c5fc0dfcc20788efe0f67c9a3c32b
KarVarr <megatr9n@gmail.com>
2023-04-02 14:17:17 +0300

    added contextMeny

--- SwiftUIHotProspects/SwiftUIHotProspects/ContentView.swift
+++ SwiftUIHotProspects/SwiftUIHotProspects/ContentView.swift
@@ -10,15 +10,30 @@ import SwiftUI
 
 
 struct ContentView: View {
-  
+    @State private var colors = Color.red
     var body: some View {
-        Image("images")
-            .interpolation(.none)
-            .resizable()
-            .scaledToFit()
-            .frame(maxHeight: .infinity)
-            .background(.mint)
-            .ignoresSafeArea()
+        VStack {
+            Text("HEy mother fucker")
+                .padding()
+                .background(colors)
+            Text("Change color")
+                .padding()
+                .background(.mint)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .contextMenu {
+                    Button(role: .destructive) {
+                        colors = . mint
+                    } label: {
+                        Label("Mint", systemImage: "circle")
+                    }
+                    
+                    Button {
+                        colors = .blue
+                    } label: {
+                        Label("Blue", systemImage: "star")
+                    }
+                }
+        }
     }
 }
 
