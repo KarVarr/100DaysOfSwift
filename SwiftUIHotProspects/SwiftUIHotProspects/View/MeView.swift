5bf887d4083cb0c41c99fe3e5ced5de6f71766dd
KarVarr <megatr9n@gmail.com>
2023-04-03 13:45:43 +0300

    added context menu

--- SwiftUIHotProspects/SwiftUIHotProspects/View/MeView.swift
+++ SwiftUIHotProspects/SwiftUIHotProspects/View/MeView.swift
@@ -12,6 +12,7 @@ import SwiftUI
 struct MeView: View {
     @State private var name = "Anonymous"
     @State private var emailAddress = "you@main.io"
+    @State private var qrCode = UIImage()
     
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
@@ -25,15 +26,30 @@ struct MeView: View {
                 TextField("Email address", text: $emailAddress)
                     .textContentType(.emailAddress)
                     .font(.title2)
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .contextMenu{
+                        Button {
+                            let imageSaver = ImageSaver()
+                            imageSaver.writeToPhotoAlbum(image: qrCode)
+                        } label: {
+                            Label("Save to Photos", systemImage: "square.and.arrow.down")
+                        }
+                    }
                     
             }
             .navigationTitle("Your QR code")
+            .onAppear(perform: updateCode)
+            .onChange(of: name) { _ in updateCode() }
+            .onChange(of: emailAddress) { _ in updateCode() }
         }
+    }
+    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
     }
     
     func generateQRCode(from string: String) -> UIImage {
@@ -41,7 +57,8 @@ struct MeView: View {
         
         if let outputImage = filter.outputImage {
             if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgimg)
+                qrCode = UIImage(cgImage: cgimg)
+                return qrCode
             }
         }
         return UIImage(systemName: "xmark.circle") ?? UIImage()
