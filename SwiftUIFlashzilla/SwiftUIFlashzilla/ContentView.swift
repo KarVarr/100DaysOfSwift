f1b8f4663e9683b66fac8bf0c02ac3e4fe8483ad
KarVarr <megatr9n@gmail.com>
2023-04-03 20:14:11 +0300

    added gester and drag

--- SwiftUIFlashzilla/SwiftUIFlashzilla/ContentView.swift
+++ SwiftUIFlashzilla/SwiftUIFlashzilla/ContentView.swift
@@ -8,37 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var currentAmountScale = 0.0
-    @State private var finalAmountScale = 1.0
-    @State private var currentAmountRotate = Angle.zero
-    @State private var finalAmountRotate = Angle.zero
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
     
     var body: some View {
-        Text("Hello world!")
-//            .scaleEffect(finalAmountScale + currentAmountScale)
-//            .gesture(
-//                MagnificationGesture()
-//                    .onChanged { amount in
-//                        currentAmountScale = amount - 1
-//                    }
-//                    .onEnded { amount in
-//                        finalAmountScale += currentAmountScale
-//                        currentAmountScale = 0
-//                    }
-//            )
-            .rotationEffect(currentAmountRotate + finalAmountRotate)
-            .gesture(
-                RotationGesture()
-                    .onChanged{ angel in
-                        currentAmountRotate = angel
-                    }
-                    .onEnded({ angel in
-                        finalAmountRotate += currentAmountRotate
-                        currentAmountRotate = .zero
-                    })
-                
-            )
-    }
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                offset = value.translation
+                isDragging = true
+            }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+            
+        let pressGesture = LongPressGesture()
+            .onEnded { value in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        
+        let combined = pressGesture.sequenced(before: dragGesture)
+        
+        Circle()
+            .fill(.mint)
+            .frame(width: 64, height: 64)
+            .shadow(radius: 5)
+            .scaleEffect(isDragging ? 1.5 : 1)
+            .offset(offset)
+            .gesture(combined)
+      }
 }
 
 struct ContentView_Previews: PreviewProvider {
