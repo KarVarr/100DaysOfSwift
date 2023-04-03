50cbd710610bba88897a75437b84d922db642634
KarVarr <megatr9n@gmail.com>
2023-04-03 19:55:54 +0300

    added gester scale and rotatioin

--- SwiftUIFlashzilla/SwiftUIFlashzilla/ContentView.swift
+++ SwiftUIFlashzilla/SwiftUIFlashzilla/ContentView.swift
@@ -10,19 +10,33 @@ import SwiftUI
 struct ContentView: View {
     @State private var currentAmountScale = 0.0
     @State private var finalAmountScale = 1.0
+    @State private var currentAmountRotate = Angle.zero
+    @State private var finalAmountRotate = Angle.zero
     
     var body: some View {
         Text("Hello world!")
-            .scaleEffect(finalAmountScale + currentAmountScale)
+//            .scaleEffect(finalAmountScale + currentAmountScale)
+//            .gesture(
+//                MagnificationGesture()
+//                    .onChanged { amount in
+//                        currentAmountScale = amount - 1
+//                    }
+//                    .onEnded { amount in
+//                        finalAmountScale += currentAmountScale
+//                        currentAmountScale = 0
+//                    }
+//            )
+            .rotationEffect(currentAmountRotate + finalAmountRotate)
             .gesture(
-                MagnificationGesture()
-                    .onChanged { amount in
-                        currentAmountScale = amount - 1
+                RotationGesture()
+                    .onChanged{ angel in
+                        currentAmountRotate = angel
                     }
-                    .onEnded { amount in
-                        finalAmountScale += currentAmountScale
-                        currentAmountScale = 0
-                    }
+                    .onEnded({ angel in
+                        finalAmountRotate += currentAmountRotate
+                        currentAmountRotate = .zero
+                    })
+                
             )
     }
 }
