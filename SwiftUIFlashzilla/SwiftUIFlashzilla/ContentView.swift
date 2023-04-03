f325fa469241ef328cd0c7be4c0f6d95a86f8a12
KarVarr <megatr9n@gmail.com>
2023-04-03 22:48:52 +0300

    added making vibrations

--- SwiftUIFlashzilla/SwiftUIFlashzilla/ContentView.swift
+++ SwiftUIFlashzilla/SwiftUIFlashzilla/ContentView.swift
@@ -4,43 +4,62 @@
 //
 //  Created by Karen Vardanian on 03.04.2023.
 //
-
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-                isDragging = true
-            }
-            .onEnded { _ in
-                withAnimation {
-                    offset = .zero
-                    isDragging = false
-                }
-            }
-            
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    isDragging = true
-                }
-            }
-        
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        Circle()
-            .fill(.mint)
-            .frame(width: 64, height: 64)
-            .shadow(radius: 5)
-            .scaleEffect(isDragging ? 1.5 : 1)
-            .offset(offset)
-            .gesture(combined)
+        Text("Hello")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
       }
+    
+    
+    func simpleSuccess() {
+        let generetor = UINotificationFeedbackGenerator()
+        generetor.notificationOccurred(.warning)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
