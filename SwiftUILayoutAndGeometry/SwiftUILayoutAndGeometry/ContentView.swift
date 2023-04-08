2b5aae23951abfc77da32ce42c606aa92fea9b69
KarVarr <megatr9n@gmail.com>
2023-04-08 22:14:51 +0300

    added rotation3d

--- SwiftUILayoutAndGeometry/SwiftUILayoutAndGeometry/ContentView.swift
+++ SwiftUILayoutAndGeometry/SwiftUILayoutAndGeometry/ContentView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            GeometryReader { fullView in
+                ScrollView {
+                    ForEach(0..<50) { index in
+                        GeometryReader { geo in
+                            Text("Row #\(index)")
+                                .font(.title)
+                                .frame(maxWidth: .infinity)
+                                .background(colors[index % 7])
+                                .shadow(radius: 5)
+                                .rotation3DEffect(.degrees(geo.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                        }
+                        .frame(height: 40)
+                    }
+                }
+            }
         }
-        .padding()
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
