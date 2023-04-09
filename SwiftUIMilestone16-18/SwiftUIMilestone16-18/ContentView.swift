9de4a152d0f71713822b7cd9b48f624039444e66
KarVarr <megatr9n@gmail.com>
2023-04-09 13:05:49 +0300

    added new project SwiftUIMilestone16-18

---
+++ SwiftUIMilestone16-18/SwiftUIMilestone16-18/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUIMilestone16-18
+//
+//  Created by Karen Vardanian on 09.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIMilestone16-18/SwiftUIMilestone16-18/SwiftUIMilestone16_18App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMilestone16_18App.swift
+//  SwiftUIMilestone16-18
+//
+//  Created by Karen Vardanian on 09.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMilestone16_18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
