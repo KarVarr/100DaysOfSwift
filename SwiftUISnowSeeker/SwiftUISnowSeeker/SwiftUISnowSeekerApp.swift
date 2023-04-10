6098e19b1218ad6e6a4711037517d927d884bba1
KarVarr <megatr9n@gmail.com>
2023-04-10 14:51:41 +0300

    added new project SwiftUISnowSeeker

---
+++ SwiftUISnowSeeker/SwiftUISnowSeeker/SwiftUISnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISnowSeekerApp.swift
+//  SwiftUISnowSeeker
+//
+//  Created by Karen Vardanian on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
