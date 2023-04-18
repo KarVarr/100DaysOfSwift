958bbe2b9803105160f4d9fa48b540528a9cfa4b
KarVarr <megatr9n@gmail.com>
2023-04-18 18:20:45 +0300

    added Project10Programmatically

---
+++ Project10Programmatically/Project10Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project10Programmatically
+//
+//  Created by Karen Vardanian on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
