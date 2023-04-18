fb70925f66c99048ac8ba853e5e3badfd1bfcf54
KarVarr <megatr9n@gmail.com>
2023-04-18 19:36:41 +0300

    added UICollectionViewController programmatically

---
+++ Project10Programmatically/Project10Programmatically/View/MyCollectionViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  MyCollectionViewCell.swift
+//  Project10Programmatically
+//
+//  Created by Karen Vardanian on 18.04.2023.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    var label: UILabel = {
+       var label = UILabel()
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        label.textAlignment = .center
+        label.textColor = .orange
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.frame = contentView.bounds
+        contentView.addSubview(label)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+}
---
+++ Project10Programmatically/Project10Programmatically/View/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Project10Programmatically
+//
+//  Created by Karen Vardanian on 18.04.2023.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+    
+    let saveKey = "cell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        
+        
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 140, height: 180)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        layout.scrollDirection = .vertical
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .orange
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        view.addSubview(collectionView)
+        
+    }
+    override init(collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(collectionViewLayout: layout)
+        
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: saveKey)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: saveKey, for: indexPath) as! MyCollectionViewCell
+        cell.label.text = "hello"
+        return cell
+    }
+    
+    
+}
+
