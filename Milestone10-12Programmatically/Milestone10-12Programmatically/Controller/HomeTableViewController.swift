5a58fbd09fa9a5c457f6779f6eef37272b63b18e
KarVarr <megatr9n@gmail.com>
2023-04-24 16:15:16 +0300

    added image and label on the cell

--- Milestone10-12Programmatically/Milestone10-12Programmatically/Controller/HomeTableViewController.swift
+++ Milestone10-12Programmatically/Milestone10-12Programmatically/Controller/HomeTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HomeTableViewController: UITableViewController {
+class HomeTableViewController: UITableViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     private let reuseIdentifier = "cell"
 
     override func viewDidLoad() {
@@ -16,10 +16,43 @@ class HomeTableViewController: UITableViewController {
         view.backgroundColor = #colorLiteral(red: 0.1960784346, green: 0.3411764801, blue: 0.1019607857, alpha: 1)
         
         tableView.register(MyTableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        
+        title = "New places"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(newPlace))
+    }
+    
+    //MARK: - Function
+    
+    @objc func newPlace() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appending(path: imageName)
+        
+        if let jpegData = image.jpegData(compressionQuality: 1) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 
     // MARK: - Table view data source
 
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        88
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
--- Milestone10-12Programmatically/Milestone10-12Programmatically/View/MyTableViewCell.swift
+++ Milestone10-12Programmatically/Milestone10-12Programmatically/View/MyTableViewCell.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class MyTableViewCell: UITableViewCell {
-
+    
+     let myImageView = UIImageView()
+     let myLabel = UILabel()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
@@ -17,6 +20,29 @@ class MyTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
+    }
+    
+    func addView() {
+        contentView.addSubview(myImageView)
+        contentView.addSubview(myLabel)
+    }
+    
+    func settings() {
+        myLabel.text = "Unknown"
+    }
+    
+    func layout() {
+        NSLayoutConstraint.activate([
+            myImageView.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            myImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
+            myImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            myImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            myImageView.widthAnchor.constraint(equalToConstant: 120),
+            myImageView.heightAnchor.constraint(equalToConstant: 100),
+            
+            myLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            myLabel.leadingAnchor.constraint(equalTo: myImageView.trailingAnchor, constant: 20),
+        ])
     }
 
 }
