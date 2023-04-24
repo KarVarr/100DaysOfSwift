26dfe03d37105dd423e2b0a8ec81c9a3b047c56d
KarVarr <megatr9n@gmail.com>
2023-04-24 21:38:49 +0300

    added Project13Programmatically

---
+++ Project13Programmatically/Project13Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project13Programmatically
+//
+//  Created by Karen Vardanian on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
