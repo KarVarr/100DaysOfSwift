386e0ec0b431ed7b5bd3fc7b9fae7e25e87c01be
KarVarr <megatr9n@gmail.com>
2023-04-27 20:03:13 +0300

    added filters and alert

--- Project13Programmatically/Project13Programmatically/Controller/ViewController.swift
+++ Project13Programmatically/Project13Programmatically/Controller/ViewController.swift
@@ -42,7 +42,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         view.addSubview(myStackViewHorizontalForButtons.stackView)
         
     }
- 
+    
     func settings() {
         title = "Instafilter"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -54,7 +54,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
         currentFilter = CIFilter(name: "CISepiaTone")
         
         mySlider.uiSlider.addTarget(self, action: #selector(applyProcessing), for: .valueChanged)
-         
+        myButtonFilter.uiButton.addTarget(self, action: #selector(changeFilter), for: .touchUpInside)
         
         myStackViewHorizontalForSlider.stackView.addArrangedSubview(myLabel.uiLabel)
         myStackViewHorizontalForSlider.stackView.addArrangedSubview(mySlider.uiSlider)
@@ -116,14 +116,44 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavi
     }
     
     @objc func applyProcessing() {
-        guard let image = currentFilter.outputImage else { return }
-        currentFilter.setValue(mySlider.uiSlider.value, forKey: kCIInputIntensityKey)
+        let inputKeys = currentFilter.inputKeys
+        let value = mySlider.uiSlider.value
         
-        if let cgimg = context.createCGImage(image, from: image.extent) {
-            let processedImage = UIImage(cgImage: cgimg)
-            myImage.uiImageView.image = processedImage
-        }
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(value, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(value * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(value * 10, forKey: kCIInputScaleKey) }
+        if inputKeys.contains(kCIInputCenterKey) { currentFilter.setValue(CIVector(x: currentImage.size.width / 2, y: currentImage.size.height / 2), forKey: kCIInputCenterKey) }
         
+        if let cgimg = context.createCGImage(currentFilter.outputImage!, from: currentFilter.outputImage!.extent) {
+                let processedImage = UIImage(cgImage: cgimg)
+            self.myImage.uiImageView.image = processedImage
+            }
+        
+    }
+    
+    @objc func changeFilter() {
+        let ac = UIAlertController(title: "Choose filter", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "CIBumpDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIGaussianBlur", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIPixellate", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CISepiaTone", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CITwirlDistortion", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIUnsharpMask", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "CIVignette", style: .default, handler: setFilter))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func setFilter(action: UIAlertAction) {
+        guard currentImage != nil else { return }
+        guard let actionTitle = action.title else { return }
+        
+        currentFilter = CIFilter(name: actionTitle)
+        
+        let beginImage = CIImage(image: currentImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        
+        applyProcessing()
     }
     
 }
