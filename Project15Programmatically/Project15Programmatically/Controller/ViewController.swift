9fecdbaeb7432c63d94471a3d563052398058672
KarVarr <megatr9n@gmail.com>
2023-04-28 19:57:34 +0300

    added animation

--- Project15Programmatically/Project15Programmatically/Controller/ViewController.swift
+++ Project15Programmatically/Project15Programmatically/Controller/ViewController.swift
@@ -28,6 +28,8 @@ class ViewController: UIViewController {
     
     func setting() {
         view.backgroundColor = .white
+        
+        buttonTap.myButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
     }
     
     func layout() {
@@ -44,6 +46,37 @@ class ViewController: UIViewController {
     }
     
     @objc func buttonTapped() {
+        buttonTap.myButton.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0,usingSpringWithDamping: 0.3, initialSpringVelocity: 5,
+            animations: {
+            switch self.currentAnimation {
+            case 0:
+                self.imagePenguin.myImage.transform = CGAffineTransform(scaleX: 2, y: 2)
+            case 1:
+                self.imagePenguin.myImage.transform = .identity
+            case 2:
+                self.imagePenguin.myImage.transform = CGAffineTransform(translationX: -100, y: -100)
+            case 3:
+                self.imagePenguin.myImage.transform = .identity
+            case 4:
+                self.imagePenguin.myImage.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            case 5:
+                self.imagePenguin.myImage.transform = .identity
+            case 6:
+                self.imagePenguin.myImage.alpha = 0.5
+                self.imagePenguin.myImage.backgroundColor = .orange
+            case 7:
+                self.imagePenguin.myImage.alpha = 1
+                self.imagePenguin.myImage.backgroundColor = .clear
+            default:
+                break
+            }
+            
+        }) { finished in
+            self.buttonTap.myButton.isHidden = false
+        }
+        
         currentAnimation += 1
         
         if currentAnimation > 7 {
