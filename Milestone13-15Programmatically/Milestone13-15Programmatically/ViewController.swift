1d6df32930e1da6bc73258673f5768d852976667
KarVarr <megatr9n@gmail.com>
2023-04-30 13:49:55 +0300

    added Milestone13-15Programmatically

---
+++ Milestone13-15Programmatically/Milestone13-15Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone13-15Programmatically
+//
+//  Created by Karen Vardanian on 30.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
