1e1c5c86103bacecd8647e29934ac25603b6a31d
KarVarr <megatr9n@gmail.com>
2023-05-05 19:00:36 +0300

    added CLLocationManager

--- Project22Programmatically/Project22Programmatically/Controller/ViewController.swift
+++ Project22Programmatically/Project22Programmatically/Controller/ViewController.swift
@@ -4,16 +4,60 @@
 //
 //  Created by Karen Vardanian on 05.05.2023.
 //
-
+import CoreLocation
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let distanceLabel = Label()
+    
+    var locationManager: CLLocationManager?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addView()
+        setting()
+        layout()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+    }
+    
+    func addView() {
+        
+        view.addSubview(distanceLabel.customLabel)
+    }
+    
+    func setting() {
+        view.backgroundColor = .orange
+        
+        distanceLabel.customLabel.text = "UNKNOWN"
+    }
+    
+    func layout() {
+        NSLayoutConstraint.activate([
+            distanceLabel.customLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            distanceLabel.customLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
     }
 
+}
 
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        if CLAuthorizationStatus.authorizedAlways == .authorizedAlways  {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    //do
+                }
+            }
+        }
+    }
+    
+    
 }
 
---
+++ Project22Programmatically/Project22Programmatically/View/Label.swift
@@ -0,0 +1,16 @@
+//
+//  Label.swift
+//  Project22Programmatically
+//
+//  Created by Karen Vardanian on 05.05.2023.
+//
+
+import UIKit
+
+class Label {
+    let customLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false 
+        return label
+    }()
+}
