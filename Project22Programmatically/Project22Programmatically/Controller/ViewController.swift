da4d9aeeda7df3c2f1ba4bd6937ac5d74998ead2
KarVarr <megatr9n@gmail.com>
2023-05-05 18:26:34 +0300

    added Project22Programmatically

---
+++ Project22Programmatically/Project22Programmatically/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22Programmatically
+//
+//  Created by Karen Vardanian on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
