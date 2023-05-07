93682311c81b9f98f645efe346aa84ff774e1c3c
KarVarr <megatr9n@gmail.com>
2023-05-07 14:33:43 +0300

    added project25

---
+++ Project25Programmatically/Project25Programmatically/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25Programmatically
+//
+//  Created by Karen Vardanian on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
