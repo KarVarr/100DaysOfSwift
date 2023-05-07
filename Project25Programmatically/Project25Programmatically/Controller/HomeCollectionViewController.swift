52effc091df1502260b5472f5dbe65dc3b6b8d71
KarVarr <megatr9n@gmail.com>
2023-05-07 18:33:53 +0300

    added MCSessionDelegate

--- Project25Programmatically/Project25Programmatically/Controller/HomeCollectionViewController.swift
+++ Project25Programmatically/Project25Programmatically/Controller/HomeCollectionViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Karen Vardanian on 07.05.2023.
 //
-
+import MultipeerConnectivity
 import UIKit
 
 private let reuseIdentifier = "Cell"
@@ -13,16 +13,24 @@ class HomeCollectionViewController: UICollectionViewController, UICollectionView
     
     var images = [UIImage]()
     
+    var peerID = MCPeerID(displayName: UIDevice.current.name)
+    var mcSession: MCSession?
+    var mcAdvertiserAssistant: MCAdvertiserAssistant?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
         collectionView.alwaysBounceVertical = true
         
+        mcSession = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        mcSession?.delegate = self
+        
+        
         title = "Selfie Share"
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
-        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showConnectionPrompt))
         
         self.collectionView!.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         
@@ -38,6 +46,29 @@ class HomeCollectionViewController: UICollectionViewController, UICollectionView
         present(picker, animated: true)
     }
     
+    
+    @objc func showConnectionPrompt() {
+        let ac = UIAlertController(title: "Connect to others", message: nil, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Host a session", style: .default, handler: startHosting))
+        ac.addAction(UIAlertAction(title: "Join a session", style: .default, handler: joinSession))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        present(ac, animated: true)
+    }
+    
+    func startHosting(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        mcAdvertiserAssistant = MCAdvertiserAssistant(serviceType: "hws-project25", discoveryInfo: nil, session: mcSession)
+        mcAdvertiserAssistant?.start()
+    }
+
+    func joinSession(action: UIAlertAction) {
+        guard let mcSession = mcSession else { return }
+        let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
+        mcBrowser.delegate = self
+        present(mcBrowser, animated: true)
+    }
+    
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.editedImage] as? UIImage else { return }
         
@@ -45,6 +76,24 @@ class HomeCollectionViewController: UICollectionViewController, UICollectionView
         
         images.insert(image, at: 0)
         collectionView.reloadData()
+        
+        guard let mcSession = mcSession else { return }
+
+        
+        if mcSession.connectedPeers.count > 0 {
+            
+            if let imageData = image.pngData() {
+                
+                do {
+                    try mcSession.send(imageData, toPeers: mcSession.connectedPeers, with: .reliable)
+                } catch {
+                    
+                    let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    present(ac, animated: true)
+                }
+            }
+        }
     }
     
     // MARK: UICollectionViewDataSource
@@ -87,6 +136,56 @@ class HomeCollectionViewController: UICollectionViewController, UICollectionView
         UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     }
 
+    
+    
+}
+
+
+extension HomeCollectionViewController: MCSessionDelegate, MCBrowserViewControllerDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+            case .connected:
+                print("Connected: \(peerID.displayName)")
+
+            case .connecting:
+                print("Connecting: \(peerID.displayName)")
+
+            case .notConnected:
+                print("Not Connected: \(peerID.displayName)")
+
+            @unknown default:
+                print("Unknown state received: \(peerID.displayName)")
+            }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        DispatchQueue.main.async { [weak self] in
+                if let image = UIImage(data: data) {
+                    self?.images.insert(image, at: 0)
+                    self?.collectionView.reloadData()
+                }
+            }
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        
+    }
+    
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
     
     
 }
